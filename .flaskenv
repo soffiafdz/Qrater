473f3a5e5b8db60e19c4ce533371233e5552f073
Soffiafdz <soffiafdz@gmail.com>
2021-05-07 20:58:09 -0400

    Main app module

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=qrater.py
+FLASK_ENV=development
---
+++ qrater.py
@@ -0,0 +1,18 @@
+"""
+Qrater.
+
+Flask webapplication for QC Neuroimaging data.
+IN DEVELOPMENT
+"""
+
+from app import create_app, db
+from app.models import Dataset, Rater, Image, Ratings
+
+app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    """Pre-import models in shell context."""
+    return {'db': db, 'Rater': Rater, 'Dataset': Dataset, 'Image': Image,
+            'Ratings': Ratings}
