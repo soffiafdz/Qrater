1b2070acd32deaa361ad33b159027ea052c3b988
Soffiafdz <soffiafdz@gmail.com>
2021-05-10 18:32:04 -0400

    Small Fixes
    Change design of progress in datasetcards;
    Fix rating filters; use dictionary
    Smaller breadcrumbs

--- app/main/routes.py
+++ app/main/routes.py
@@ -9,9 +9,10 @@ import re
 import csv
 import json
 from shutil import rmtree
+from collections import defaultdict
 from datetime import datetime
 from werkzeug.utils import secure_filename
-from flask import (render_template, flash, redirect, url_for, request,
+from flask import (render_template, flash, abort, redirect, url_for, request,
                    current_app, send_file)
 from flask_login import current_user, login_required
 from app import db
@@ -32,13 +33,17 @@ def before_request():
 
 @bp.route("/", methods=['GET', 'POST'])
 @bp.route("/dashboard", methods=['GET', 'POST'])
-def dashboard():
+@bp.route("/dashboard_<int:all_raters>", methods=['GET', 'POST'])
+def dashboard(all_raters=None):
     """Construct the landing page."""
+    if all_raters is not None and all_raters > 1:
+        abort(404)
     if Dataset.query.first() is None:
         return render_template('no_datasets.html', title='Dashboard',
                                rater=current_user)
-    n_tot, npass, npass100, nwarn, nwarn100, nfail, nfail100, npend = \
-        [], [], [], [], [], [], [], []
+    # keys = ["total", "pass", "pass100", "warning", "warning100", "fail",
+    # "fail100", "pending"]
+    n_imgs = defaultdict(list)
     for dataset in Dataset.query.all():
         imgs = dataset.images
         ntot = imgs.count()
@@ -47,39 +52,37 @@ def dashboard():
         n_1 = imgs.join(Ratings).filter_by(rating=1).count()
         n_2 = imgs.join(Ratings).filter_by(rating=2).count()
         n_3 = imgs.join(Ratings).filter_by(rating=3).count()
-        n_tot.append(ntot)
-        npend.append(n_0)
-        npass.append(n_1)
-        nwarn.append(n_2)
-        nfail.append(n_3)
-        npass100.append(round(n_1 / ntot * 100))
-        nwarn100.append(round(n_2 / ntot * 100))
-        nfail100.append(round(n_3 / ntot * 100))
+        n_imgs['total'].append(ntot)
+        n_imgs['pending'].append(n_0)
+        n_imgs['pass'].append(n_1)
+        n_imgs['warning'].append(n_2)
+        n_imgs['fail'].append(n_3)
+        n_imgs['pass100'].append(round(n_1 / ntot * 100))
+        n_imgs['warning100'].append(round(n_2 / ntot * 100))
+        n_imgs['fail100'].append(round(n_3 / ntot * 100))
+        print(n_imgs)
     return render_template('dashboard.html', title='Dashboard',
                            Image=Image, Ratings=Ratings,
                            datasets=Dataset.query.all(),
-                           npend=npend, n_tot=n_tot,
-                           npass=npass, npass100=npass100,
-                           nwarn=nwarn, nwarn100=nwarn100,
-                           nfail=nfail, nfail100=nfail100)
+                           n_imgs=n_imgs)
 
 
 @bp.route('/<dataset>', methods=['GET', 'POST'])
-@bp.route('/<dataset>/filter-<int:filter>', methods=['GET', 'POST'])
+@bp.route('/<dataset>/filter-<int:r_filter>', methods=['GET', 'POST'])
 @login_required
-def rate(dataset, filter=None):
+def rate(dataset, r_filter=None):
     """Page to view images and rate them."""
     DS = Dataset.query.filter_by(name=dataset).first_or_404()
     page = request.args.get('page', 1, type=int)
-    if filter is None:
+    if r_filter is None:
         imgs = DS.images.order_by(Image.id.asc()).paginate(page, 1, False)
-    elif filter == 0:
+    elif r_filter == 0:
         unrated = DS.images.filter_by(ratings=None)
-        pending = DS.images.join(Ratings).filter_by(rating=filter)
+        pending = DS.images.join(Ratings).filter_by(rating=r_filter)
         imgs = unrated.union(pending).order_by(Image.id.asc()).paginate(
             page, 1, False)
-    elif filter < 4:
-        imgs = DS.images.join(Ratings).filter_by(rating=filter).order_by(
+    elif r_filter < 4:
+        imgs = DS.images.join(Ratings).filter_by(rating=r_filter).order_by(
             Image.id.asc()).paginate(page, 1, False)
     else:
         flash('Invalid filtering; Showing all images...', 'danger')
@@ -88,7 +91,6 @@ def rate(dataset, filter=None):
         flash('Filter ran out of images in filter; Showing all images...',
               'info')
         return redirect(url_for('main.rate', dataset=dataset))
-    # TODO: This changes DSET_PATH to static... FIX
     path = imgs.items[0].path.replace("app/static/", "")
     form = RatingForm()
     if form.validate_on_submit():
@@ -97,7 +99,7 @@ def rate(dataset, filter=None):
         img.set_comment(user=current_user, comment=form.comment.data)
         return redirect(request.url)
     return render_template('rate.html', DS=DS, form=form, imgs=imgs, pag=True,
-                           filter=filter, img_path=(path),
+                           img_path=(path),
                            img_name=imgs.items[0].name,
                            comment=imgs.items[0].comment_by_user(current_user),
                            rating=imgs.items[0].rating_by_user(current_user))
