bba8193084c9d22b1d89883bdba391d426636f05
Soffiafdz <soffiafdz@gmail.com>
2021-05-25 00:10:56 -0400

    Add type column/hide useless columns

--- app/dt/routes.py
+++ app/dt/routes.py
@@ -6,7 +6,6 @@ Module with blueprint specific routes
 
 from flask import jsonify, request, render_template
 from flask_login import current_user
-from sqlalchemy import or_
 from datatables import ColumnDT, DataTables
 from app.models import db, Dataset, Image, Ratings, Rater
 from app.dt import bp
@@ -16,34 +15,46 @@ from app.dt import bp
 def datatable(dataset):
     """List a table with the images and their ratings."""
     # Figure out how to send Dataset
-    ds_mod = Dataset.query.filter_by(name=dataset).first_or_404()
+    ds_model = Dataset.query.filter_by(name=dataset).first_or_404()
     all_raters = request.args.get('all_raters', 0, type=int)
     only_ratings = request.args.get('only_ratings', 0, type=int)
 
-    subs = [i.subject for i in ds_mod.images.all()]
-    sub_labs = (subs.count(None) != len(subs))
+    col_names = ['Type', 'Sub', 'Sess', 'Ratings']
+    columns = {**dict.fromkeys(col_names, None)}
 
-    sess = [i.session for i in ds_mod.images.all()]
-    sess_labs = (sess.count(None) != len(sess))
+    types = [i.imgtype for i in ds_model.images.all()]
+    columns['Type'] = (types.count(None) != len(types))
 
-    return render_template("dt/datatable.html", DS=ds_mod,
-                           all_raters=all_raters, only_ratings=only_ratings,
-                           sub_labs=int(sub_labs), sess_labs=int(sess_labs))
+    subs = [i.subject for i in ds_model.images.all()]
+    columns['Sub'] = (subs.count(None) != len(subs))
+
+    sess = [i.session for i in ds_model.images.all()]
+    columns['Sess'] = (sess.count(None) != len(sess))
+
+    ratings_lists = [i.ratings.all() for i in ds_model.images.all()]
+    columns['Ratings'] = (ratings_lists.count([]) != len(ratings_lists))
+
+    return render_template("dt/datatable.html", DS=ds_model,
+                           types=columns['Type'], subs=columns['Sub'],
+                           sess=columns['Sess'], ratings=columns['Ratings'],
+                           all_raters=all_raters, only_ratings=only_ratings)
 
 
-@bp.route('/data/<int:dset_id>/<int:subject>/<int:session>/<int:only_ratings>')
-def data(dset_id, subject, session, only_ratings=0):
+@bp.route('/data/<int:dset_id>/<int:type>/<int:subject>/' +
+          '<int:session>/<int:ratings>/<int:only_ratings>')
+def data(dset_id, type, subject, session, ratings, only_ratings=0):
     """Return server side data for datatable."""
     all_raters = request.args.get('all_raters', 0, type=int)
 
     columns = [
         ColumnDT(Image.name),
         ColumnDT(Ratings.rating),
-        ColumnDT(Ratings.timestamp),
     ]
 
-    # If ALL_RATERS, add rater column
-    columns.insert(2, ColumnDT(Rater.username))
+    # If there are ratings insert rating info
+    if ratings:
+        columns.insert(2, ColumnDT(Rater.username))
+        columns.insert(3, ColumnDT(Ratings.timestamp))
 
     # Check if there are sess labels
     if session:
@@ -52,6 +63,10 @@ def data(dset_id, subject, session, only_ratings=0):
     # Check if there are sub labels
     if subject:
         columns.insert(1, ColumnDT(Image.subject))
+
+    # Check if there are type labels
+    if type:
+        columns.insert(1, ColumnDT(Image.imgtype))
 
     # Ratings query for single user (only ratings)
     if not only_ratings:
