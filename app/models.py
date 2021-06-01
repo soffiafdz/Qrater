1cf0eeed1394e33dbbcae844a556c89f137191ca
Soffiafdz <soffiafdz@gmail.com>
2021-06-01 13:25:41 -0400

    Fix POSSIBLE bug with jwt.encode()

--- app/models.py
+++ app/models.py
@@ -42,7 +42,7 @@ class Rater(UserMixin, db.Model):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
             current_app.config['SECRET_KEY'],
-            algorithm='HS256').decode('utf-8')
+            algorithm='HS256')
 
     @staticmethod
     def verify_reset_password_token(token):
