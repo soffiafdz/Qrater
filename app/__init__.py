17d17e52f463d8766296752e0e28421de6b62f3b
sfernandez <sofia.fernandezlozano@mail.mcgill.ca>
2021-07-15 16:36:10 -0400

    Added batch mode to migrations for last upgrade

--- app/__init__.py
+++ app/__init__.py
@@ -35,7 +35,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate.init_app(app, db, render_as_batch=True)
     login.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
