697e5d96209dd7b042a4e39168c5e4a13a98399d
sfernandez <sofia.fernandezlozano@mail.mcgill.ca>
2021-07-22 22:37:04 -0400

    Environment variables change
    Move the environment variables that shouldn't be changed by the user
    into the compose yaml. The qrater.env file should only content the
    variables that the user can/should declare.

--- config.py
+++ config.py
@@ -51,7 +51,8 @@ class Config():
     REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or False
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') or False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = mail_password or os.environ.get('MAIL_PASSWORD')
     ADMINS = os.environ.get("ADMINS_MAIL").split(",") if \
